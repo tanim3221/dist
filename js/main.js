bb23fe173299dfce088d188147308ba5c2556753
SAKLAYEN TANIM <astanim.3221@gmail.com>
2020-11-22 21:30:52 +0600

    toastr css and js edit.

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@
             "onclick": null,
             "showDuration": "300",
             "hideDuration": "1000",
-            "timeOut": "5000",
+            "timeOut": "15000",
             "extendedTimeOut": "1000",
             "showEasing": "swing",
             "hideEasing": "linear",
